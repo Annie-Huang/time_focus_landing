cf85e48bbe6cf3b242204f87a0d347ea8db990f9
Annie Huang <annie.hn@gmail.com>
2019-11-10 11:08:34 +1100

    Show am and pm next to the time.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ const time = document.getElementById('time'),
     name = document.getElementById('name'),
     focus = document.getElementById('focus');
 
+// Options
+const showAmPm = true;
+
 // Show Time
 function showTime() {
     // Set to night time to test.
@@ -11,7 +14,7 @@ function showTime() {
     let today = new Date(),
         hour = today.getHours(),
         min = today.getMinutes(),
-        second = today.getSeconds();
+        sec = today.getSeconds();
 
     // Set AM or PM
     const amPm = hour >= 12 ? 'PM' : "AM";
@@ -22,7 +25,7 @@ function showTime() {
 
     // Output Time
     // time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${second}`;
-    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(second)}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
 
     setTimeout(showTime, 1000);
 }
